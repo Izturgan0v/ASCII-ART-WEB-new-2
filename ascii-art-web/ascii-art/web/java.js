102e66e3c61bee17112b01022b2bbcf845fe0331
Izturganov <146101263+Izturgan0v@users.noreply.github.com>
2025-08-07 16:14:45 +0500

    GG

---
+++ ascii-art-web/ascii-art/web/java.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById('generate-btn');
+
+btn.addEventListener('click', function(){
+    alert('Click Click Click')
+});
---
+++ ascii-art-web/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	asciiart "ascii-art-web/ascii-art"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+//---------------------------------------------------------------------------------------|
+
+var errFile string
+
+func main() {
+	content, err := os.ReadFile("templates/error.html")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	errFile = string(content)
+
+	ip := "10.25.0.59"
+	port := 45674
+
+	http.HandleFunc("/", handlerHome)
+	http.HandleFunc("/sabik", handlerSabik)
+	http.HandleFunc("/ascii-art", handlerAsciiart)
+
+	addr := fmt.Sprintf("%s:%d", ip, port)
+	fmt.Printf("server runing http://%s\n", addr)
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+//---------------------------------------------------------------------------------------|
+
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+	indexPage, err := os.ReadFile("templates/index.html")
+	if err != nil {
+		handlerError(w, http.StatusInternalServerError)
+		return
+	}
+	// нужен обработчик неизвестных путей
+
+	if r.URL.Path != "/" {
+		handlerError(w, http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, string(indexPage))
+}
+
+//---------------------------------------------------------------------------------------|
+
+func handlerSabik(w http.ResponseWriter, r *http.Request) {
+	sabikPage, err := os.ReadFile("templates/sabik.html")
+	if err != nil {
+		handlerError(w, http.StatusInternalServerError)
+		return
+	}
+
+	// нужен обработчик неизвестных путей
+
+	fmt.Fprint(w, string(sabikPage))
+
+}
+
+//---------------------------------------------------------------------------------------|
+
+func handlerAsciiart(w http.ResponseWriter, r *http.Request) {
+	text := r.FormValue("text")
+	banner := r.FormValue("banner")
+
+	// проверить что text и банер не пустые
+	// проверить что банер существует
+	asciiPage, err := os.ReadFile("templates/asciiart.html")
+	if err != nil {
+		handlerError(w, http.StatusInternalServerError)
+		return
+	}
+	result, err := asciiGenerator(text, banner)
+	if err != nil {
+		handlerError(w, http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, string(asciiPage), result)
+}
+
+//---------------------------------------------------------------------------------------|
+
+func handlerError(w http.ResponseWriter, errCode int) {
+	fmt.Fprintf(w, errFile, errCode)
+}
+
+//---------------------------------------------------------------------------------------|
+
+func asciiGenerator(text, banner string) (string, error) {
+
+	asciiArt, err := asciiart.Generate(text, banner)
+	if err != nil {
+		return "", err
+	}
+
+	return asciiArt, nil
+}
